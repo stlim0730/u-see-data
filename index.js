c1fd74528bbfa7d788c710a0ce046fab4a60c18d
Seongtaek Lim <EH71@airbears2-10-142-132-122.AirBears2.1918.Berkeley.EDU>
2015-03-02 16:55:09 -0800

    groundwork: node server with uploading feature

--- index.js
+++ index.js
@@ -2,23 +2,54 @@
 var conf;
 if (process.argv.length >= 2 + 1) {
   conf = require(process.argv[2]);
-} else {
+}
+else {
   conf = require('./default_conf.json');
 }
 
-// get global varables from conf
+// set global varables
 var port_number = conf.port_number;
+var public_dir = __dirname + conf.public_dir;
+var bower_components_dir = __dirname + conf.bower_components_dir;
+var upload_dir = __dirname + conf.upload_dir;
 
 // load modules
 var express = require('express');
+var multer  = require('multer');
+var fs = require('fs');
 var util = require('./my_util.js');
 
-// start the server
+// set the server
 var app = express();
+app.use('/public', express.static(public_dir));
+app.use('/bower_components', express.static(bower_components_dir));
+app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-  res.end('Hello, World!');
+app.get('/', function (req, res) {
+  res.render('index.ejs'); // res.render('index', { title: 'Hey', message: 'Hello there!'}); // this is how to use templates
 });
 
+app.get('/renderer', function (req, res) {
+  res.render('renderer.ejs');
+});
+
+app.post('/data_upload', [
+  multer({ dest: upload_dir }),
+  function (req, res) {
+    var user_file = req.files.user_data;
+    // TODO: file management per user
+    fs.unlink(user_file.originalname, function (err) { // remove existing file with the same temporary file name
+      fs.rename(upload_dir + '/' + user_file.name,
+        upload_dir + '/' + user_file.originalname,
+        function (err) {
+          if (err) throw err;
+          console.log('uploading complete');
+          res.redirect('/renderer');
+      });
+    });
+  }
+]);
+
+// start the server
 app.listen(port_number);
 util.log('the server is running on ' + port_number);
---
+++ views/js/init.js
@@ -0,0 +1,3 @@
+$('#file_input').on('change', function (event) {
+  $('#upload_form').submit();
+});
