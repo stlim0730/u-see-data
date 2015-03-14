f6137491ed3c9f8a1987830fabaeb3d76a543047
Seongtaek Lim <EH71@Seongtaeks-MacBook-Pro.local>
2015-03-13 23:59:19 -0700

    Included mongoose node module

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ else {
   conf = require('./default_conf.json');
 }
 
-// set global varables
-var port_number = conf.port_number;
+// set directories
 var public_dir = __dirname + conf.public_dir;
 var bower_components_dir = __dirname + conf.bower_components_dir;
 var upload_dir = __dirname + conf.upload_dir;
@@ -18,14 +17,32 @@ var express = require('express');
 var multer  = require('multer');
 var fs = require('fs');
 var util = require('./my_util.js');
+var mongoose = require ('mongoose');
 
 // set the server
 var app = express();
-app.set('port', (process.env.PORT || port_number));
+app.set('port', (process.env.PORT || conf.port_number));
 app.use('/public', express.static(public_dir));
 app.use('/bower_components', express.static(bower_components_dir));
 app.set('view engine', 'ejs');
 
+// set global varables
+var port_number = app.get('port');
+var db_uri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  conf.db_uri;
+
+// set database
+mongoose.connect(db_uri, function (err, res) {
+  if (err) {
+    util.log ('Error in connecting to Mongoose: ' + db_uri + '. ' + err);
+  }
+  else {
+    util.log('Successfully connected to Mongoose: ' + db_uri);
+  }
+});
+
+// routing definitions
 app.get('/', function (req, res) {
   res.render('index.ejs'); // res.render('index', { title: 'Hey', message: 'Hello there!'}); // this is how to use templates
 });
@@ -52,5 +69,5 @@ app.post('/data_upload', [
 ]);
 
 // start the server
-app.listen(process.env.PORT || port_number);
+app.listen(port_number);
 util.log('the server is running on ' + port_number);
