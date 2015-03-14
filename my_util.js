f45bf1dc3c47dcbe4a9904af0d45b20187cbfc1a
Seongtaek Lim <EH71@Seongtaeks-MacBook-Pro.local>
2015-03-14 16:18:31 -0700

    Datatype detection

--- my_util.js
+++ my_util.js
@@ -9,3 +9,16 @@ var get_time_str = function () {
 exports.log = function (text) {
   console.log('[' + packageInfo.name + ' log ' + get_time_str() + '] - ' + text);
 };
+
+exports.objectLength = function (obj) {
+  var cnt = 0; for(key in obj) cnt++; return cnt;
+};
+
+exports.arrayContains = function (arr, obj) {
+  for (i=0; i < arr.length; i++) {
+    if (arr[i] == obj) {
+      return true;
+    }
+  }
+  return false;
+}
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// AUTHOR: SEONGTAEK LIM
+
 // read configuration json file
 var conf;
 if (process.argv.length >= 2 + 1) {
@@ -64,6 +66,7 @@ app.post('/data_upload', [
   function (req, res) {
     var user_file = req.files.user_data;
     // TODO: limit file size?
+    // TODO: validate the csv file: missing values, missing columns
     // TODO: file management per user: manipulate directory name for data repository
     fs.unlink(user_file.originalname, function (err) { // remove existing file with the same temporary file name
       var random_file_name = upload_dir + '/' + user_file.name;
@@ -74,34 +77,84 @@ app.post('/data_upload', [
           util.log('uploading complete');
           
           // parse csv file
-          var header = [];
+          var col_names = [];
+          var col_types = [];
           var row_num = -1;
           var fstream = fs.createReadStream(file_name);
-          var parser = csv(require('./default_csv_conf.json'), function () {
+          var parser = csv(require('./default_csv_conf.json'), function (err, doc) {
+            if (err) throw err;
+            
+            // aggregate datatypes
+            // TODO: polish datatype rule
+            for (var i=0; i < col_types.length; i++) {
+              var types = col_types[i];
+              if (types.length == 1) {
+                // homogeneous datatype
+                col_types[i] = types[0];
+              }
+              else if (util.arrayContains(types, typeof 'string')) {
+                // if there is one or more string as a value for this column, the column's datatype is string.
+                col_types[i] = typeof 'string';
+              }
+              else {
+                util.log('Unexpected datatype');
+                col_types[i] = 'undefined';
+              }
+            }
+
             // update database after parsing
             var new_dataset_rep = new Dataset({
               user: 'user', // TODO: user identifier
               date: new Date(), // TODO: timezone resolution: currently it uses UTC
               path: file_name,
               file_size: user_file.size,
-              col_num: header.length,//Number,
-              row_num: row_num,//Number,
-              col_names: header,//Array,
-              col_types: []//Array
+              col_num: col_names.length,
+              row_num: row_num,
+              col_names: col_names,
+              col_types: col_types
             });
             new_dataset_rep.save(function (err, dataset) {
               if (err) throw err;
+
               util.log('The representation of ' + dataset.path + ' has been saved in database.');
             });
           });
           parser.on('readable', function () {
             var record = parser.read();
             row_num = parser.lineNo; // starts from 1
+            
+            // extract column names
             if (row_num == 1) {
               for (key in record) {
-                header.push(key.trim());
+                col_names.push(key.trim());
+                col_types.push([]);
               }
             }
+
+            // extracts all the possible datatypes
+            if (record)
+              for (var i=0; i < col_names.length; i++) {
+                var key = col_names[i];
+                var value = record[key];
+                if (!value) continue; // skip missing values
+                value = value.trim();
+                var type;
+
+                if (isNaN(value)) {
+                  // likely string
+                  type = typeof 'string';
+                }
+                else {
+                  // number
+                  type = typeof 123;
+                }
+                // console.log(value + ', ' + type + ', ' + isNaN(value));
+
+                // keep it if it's new
+                if (!util.arrayContains(col_types[i], type)) {
+                  col_types[i].push(type);
+                }
+              }
           });
           fstream.pipe(parser);
 
